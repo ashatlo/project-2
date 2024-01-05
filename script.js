d40a6d2439d6034042feb287257d99c15ac839bf
ashatlo <ahmad-barca@live.com>
2024-01-05 07:14:12 +0100

    finished  project

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function toggleMenu(){
     icon.classList.toggle("open");
 
 }
+function sendEmail() {
+    emailjs.send("smtp_server", "template_id", {
+        to_email: 'them@website.com',
+        from_email: "you@isp.com",
+        subject: "This is the subject",
+        body: "And this is the body"
+    })
+    .then(function(response) {
+        console.log('Email sent successfully:', response);
+        alert('Email sent successfully!');
+    }, function(error) {
+        console.log('Email sending failed:', error);
+        alert('Email sending failed. Please try again later.');
+    });
+}
